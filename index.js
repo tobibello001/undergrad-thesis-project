1c76a54e7ed58a2f385c16f89f81a5fff6f9822b
Tobi Bello <bello.tobi@outlook.com>
2018-07-13 02:36:48 +0100

    extracted body tag without class and id attribute

--- index.js
+++ index.js
@@ -1,43 +1,17 @@
-const https = require("https");
-const http = require("http");
 const cheerio = require("cheerio");
-const getHTMLData = url => {
-    return new Promise((resolve, reject) => {
-        https.get(url, (res) => {
-            const { statusCode } = res;
-            const contentType = res.headers['content-type'];
+const fs = require("fs");
+const htmlValidator = require("html-validator");
 
-            let error;
-            if (statusCode !== 200) {
-                error = new Error('Request Failed.\n' +
-                    `Status Code: ${statusCode}`);
-            } else if (!/^text\/html/.test(contentType)) {
-                error = new Error('Invalid content-type.\n' +
-                    `Expected application/json but received ${contentType}`);
-            }
-            if (error) {
-                reject(error);
-                // consume response data to free up memory
-                res.resume();
-            }
-
-            res.setEncoding('utf8');
-            let rawData = '';
-            res.on('data', (chunk) => {
-                rawData += chunk;
-            });
-            res.on('end', () => {
-                const parsedData = rawData;
-                resolve(parsedData);
-            });
-        }).on('error', (e) => {
-            reject(e);
-        });
-    });
-}
-
-getHTMLData('https://unilag.edu.ng/news/').then((htmlData) => {
-    console.log(htmlData);
-}).catch((error) => {
-    console.error(`Got error: ${error.message}`);
+fs.readFile("body.html", 'utf8', (err, body) => {
+    // console.log(htmlData);
+    if (err) {
+        console.error(`Got error: ${err.message}`);
+    }
+    const bodyNoClassOrIdAttr = body.replace(/(\s+(class|id|style|data-.+?)\s*=\s*".*?"|<(style|script|![--]?)[\s\S]*?[<\/]?(style|script|--)>)/g, "");
+    fs.writeFile("bodyNoClassOrIdAttr.html", bodyNoClassOrIdAttr, (err) => {
+        if (err) {
+            console.error(`Got error: ${err.message}`);
+        }
+        console.log("Body Saved");
+    })
 })
