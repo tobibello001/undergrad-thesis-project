1e068bf6725fc89de8495fefc9309334f5058015
Tobi Bello <bello.tobi@outlook.com>
2018-08-09 02:05:15 +0100

    to use request for http requesting

--- index.js
+++ index.js
@@ -4,51 +4,9 @@ const fs = require("fs");
 const cheerio = require("cheerio");
 const sanitizeHtml = require('sanitize-html');
 const mongoose = require('mongoose');
+const request = require('request');
 
 const Post = require('./models/posts');
-
-mongoose.connect('mongodb://localhost/UnilagNewsPosts', { autoIndex: false })
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch(err => {
-        return console.error(err);
-    });
-
-const getHTMLData = url => {
-    return new Promise((resolve, reject) => {
-        https.get(url, (res) => {
-            const { statusCode } = res;
-            const contentType = res.headers['content-type'];
-
-            let error;
-            if (statusCode !== 200) {
-                error = new Error('Request Failed.\n' +
-                    `Status Code: ${statusCode}`);
-            } else if (!/^text\/html/.test(contentType)) {
-                error = new Error('Invalid content-type.\n' +
-                    `Expected application/json but received ${contentType}`);
-            }
-            if (error) {
-                reject(error);
-                // consume response data to free up memory
-                res.resume();
-            }
-
-            res.setEncoding('utf8');
-            let rawData = '';
-            res.on('data', (chunk) => {
-                rawData += chunk;
-            });
-            res.on('end', () => {
-                const parsedData = rawData;
-                resolve(parsedData);
-            });
-        }).on('error', (e) => {
-            reject(e);
-        });
-    });
-}
 
 const writeTextToFile = (filename, text) => {
     fs.writeFile(filename, text, (err) => {
@@ -59,46 +17,53 @@ const writeTextToFile = (filename, text) => {
     })
 }
 
-getHTMLData('https://unilag.edu.ng/news/')
-    .then((htmlData) => {
-        cleanHtmlData = sanitizeHtml(htmlData, { allowedAttributes: false, allowedTags: false });
-
-        let filtered;
-        filtered = sanitizeHtml(cleanHtmlData, {
-            allowedTags: [...sanitizeHtml.defaults.allowedTags, 'h1', 'h2', 'img', 'body', 'span'],
-            allowedClasses: { 'h2': ['entry-title'], 'div': ['post', 'fusion-post-content-container'], 'span': ['updated'] },
-        });
-
-        let $ = cheerio.load(filtered);
-
-        let htmlOFPosts = [];
-        $("div.post").each((i, ele) => {
-            dhtml = $(ele).html()
-            htmlOFPosts.push(dhtml);
-        })
-
-        let posts = htmlOFPosts.map(value => {
-            let postDetails = {}
-            $ = cheerio.load(value);
-            postDetails.title = $('h2.entry-title a').text()
-            postDetails.link = $('h2.entry-title a').attr('href')
-            postDetails.imageLink = $('img').attr('src') || null;
-            // postDetails.shortDescription = $('div.fusion-post-content-container p').text()
-            postDetails.updated = new Date($('span.updated').text())
-            return postDetails;
-        })
-        posts.sort((a, b) => b.updated - a.updated);
-
-        posts.forEach(post => {
-            let postDoc = new Post(post);
-            postDoc.save(function (err) {
-                if (err) return console.error(err);
-                console.log(`${postDoc.title} Saved`);
-            })
-        });
+mongoose.connect('mongodb://localhost:27017/UnilagNewsPosts', { autoIndex: false, useNewUrlParser: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
     })
-    .catch((err) => {
-        console.dir(err);
-        console.error(`Got error: ${err.message}`);
+    .catch(err => {
+        return console.error(err);
+    });
+
+
+
+request('https://unilag.edu.ng/news/', (err, response, body) => {
+
+    if (err) console.error(`Got error: ${err.message}`);
+
+    cleanHtml = sanitizeHtml(body, { allowedAttributes: false, allowedTags: false });
+
+    let filtered;
+    filtered = sanitizeHtml(cleanHtml, {
+        allowedTags: [...sanitizeHtml.defaults.allowedTags, 'h1', 'h2', 'img', 'body', 'span'],
+        allowedClasses: { 'h2': ['entry-title'], 'div': ['post', 'fusion-post-content-container'], 'span': ['updated'] },
+    });
+
+    let $ = cheerio.load(filtered);
+
+    let htmlOFPosts = [];
+    $("div.post").each((i, ele) => {
+        dhtml = $(ele).html()
+        htmlOFPosts.push(dhtml);
     })
 
+    let posts = htmlOFPosts.map(value => {
+        let postDetails = {}
+        $ = cheerio.load(value);
+        postDetails.title = $('h2.entry-title a').text()
+        postDetails.link = $('h2.entry-title a').attr('href')
+        postDetails.imageLink = $('img').attr('src') || null;
+        // postDetails.shortDescription = $('div.fusion-post-content-container p').text()
+        postDetails.updated = new Date($('span.updated').text())
+        return postDetails;
+    })
+    posts.sort((a, b) => b.updated - a.updated);
+
+    posts.forEach(post => {
+        let postDoc = new Post(post);
+        postDoc.save(function (err) {
+            if (err) return console.error(err);
+            console.log(`${postDoc.title} Saved`);
+        })
+    });
+})
